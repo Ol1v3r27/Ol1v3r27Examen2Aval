afa3aa24ec8b98acc9e865077dddaa468bd0edd0
Óliver Puchades Pinel <114495024+Ol1v3r27@users.noreply.github.com>
2023-02-14 16:57:10 +0100

    d.sh

---
+++ deco.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo ""
+read -p "Escribe una ciudad de España: " ciudad
+while [ $ciudad != 'Valencia' ] && [ $ciudad != 'Barcelona' ] && [ $ciudad != 'Madrid' ]; do
+    echo ""
+    echo "Debes escribir Valencia, Barcelona o Madrid"
+    echo ""
+    read -p "Escribe una ciudad de España: " ciudad
+done
+
+echo ""
+
+contador=$(cat consumos.txt | grep $ciudad | awk '{ print $4 }' | wc -l)
+for ((i = 1; i <= $contador; i++)); do
+    ciudades=$(cat consumos.txt | grep $ciudad | head -n$i | tail -n1 | awk '{ print $4 }')
+    sumatorio=$((sumatorio + ciudades))
+done
+echo "Consumo total de $ciudad: $sumatorio"
+media=$((sumatorio/contador))
+echo "La media es $media"
+if [ $media -lt 400 ];
+then
+echo "$ciudad es ECO"
+elif [ $media -ge 400 ];
+then
+echo "$ciudad no es ECO"
+fi
